256286f1e3122be697c880590d04d233e0bf3b46
梁典典 <413153189@qq.com>
2025-11-25 09:20:36 +0800

    initial commit

---
+++ example/android/app/src/main/kotlin/com/example/dpython_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dpython_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Classes/dpython.c
@@ -0,0 +1,3 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../dpython.podspec for more information.
+#include "../../src/dpython.c"
